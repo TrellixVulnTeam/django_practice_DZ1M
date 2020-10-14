31b53064e1f8afc87ce0b2092462771156b4c615
wooseok1223 <tjdntjr123@gmail.com>
2020-10-14 15:22:39 +0900

    수정

--- blog1/models.py
+++ blog1/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.conf import settings
 
 class Post(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
+                              related_name='+')
     content = models.TextField()
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
