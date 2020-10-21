4b8c1cd1aa129c8b3166e5ed7ac6535c8f65fa06
wooseok1223 <tjdntjr123@gmail.com>
2020-10-21 15:30:31 +0900

    django url, view test

--- instagram/models.py
+++ instagram/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.conf import settings
-
+from django.urls import reverse
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
@@ -14,6 +14,9 @@ class Post(models.Model):
     def __str__(self):
         # return f"Custom Post object ({self.id})"
         return self.message
+
+    def get_absolute_url(self):
+        return reverse('instagram:post_detail', args=[self.pk])
 
     class Meta:
         ordering = ['-id']
--- instagram/views.py
+++ instagram/views.py
@@ -1,12 +1,24 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, get_object_or_404
+from django.utils.decorators import method_decorator
+
 from .models import Post
 from django.http import HttpResponse, Http404
-from django.views.generic import ListView, DetailView
-
-post_list = ListView.as_view(model=Post, paginate_by=10)
+from django.views.generic import ListView, DetailView, ArchiveIndexView, YearArchiveView, MonthArchiveView
 
 
+#
+# post_list = login_required(ListView.as_view(model=Post, paginate_by=10))
 
+# @method_decorator(login_required, name='dispatch')
+class PostListView(LoginRequiredMixin, ListView):
+    model = Post
+    paginate_by = 10
+
+post_list = PostListView.as_view()
+
+# @login_required
 # def post_list(request):
 #     qs = Post.objects.all()
 #     q = request.GET.get('q', '')
@@ -47,6 +59,12 @@ class PostDetailView(DetailView):
 
 post_detail = PostDetailView.as_view()
 
-def archives_year(request, year):
+# def archives_year(request, year):
+#
+#     return HttpResponse(f"{year}년 archives")
 
-    return HttpResponse(f"{year}년 archives")
+post_archive = ArchiveIndexView.as_view(model=Post, date_field='created_at', paginate_by=10)
+
+post_archive_year = YearArchiveView.as_view(model=Post, date_field='created_at', make_object_list=True)
+
+post_archive_month = MonthArchiveView.as_view(model=Post, date_field='created_at', month_format='%m')
