2cd3e8feaf2d97ca7f61e2207d0660fa3606dfcb
wooseok1223 <tjdntjr123@gmail.com>
2020-10-27 14:32:20 +0900

    장고 어드민/ form 을 이용한 기본 구현

--- askcompany/settings.py
+++ askcompany/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from django.urls import reverse_lazy
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -114,7 +116,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+LOGIN_REDIRECT_URL = reverse_lazy('login')
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
